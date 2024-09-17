7f7960cc699feb85dd055e9b6989726a9dbb46af
parkerwind05 <parkerwind05@gmail.com>
2024-09-17 15:06:49 -0400

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        int age = 18;
+        double salary = 100000;
+        String name = "Parker";
+        System.out.println("Hey " + name);
+
+        System.out.println("Hello " + name);
+        System.out.println("You said your age is " + age);
+        System.out.println("You said your salary is " + salary);
+    }
+}
